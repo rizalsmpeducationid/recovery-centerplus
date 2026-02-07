9a231bd259cbe5eca9c8d56877439bb653d849ae
rizalsmpeducationid <rizal.ahmad8@smp.belajar.id>
2026-02-07 09:19:02 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/recovery-center/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/recovery-centerplus/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
